94d81ca92ca92d203e04c7856946f716a3ec7e25
Nikita.L <nlomonosov@gmail.com>
2022-09-18 19:11:53 +0300

    Добавлена программа и исправлена блок-схема

--- Program.cs
+++ Program.cs
@@ -6,3 +6,29 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+Console.Clear();
+Console.Write("Введите элементы исходного массива строк через пробел: ");
+string[] initArray = Console.ReadLine().Split(new char[] { ' ' });
+
+int i2=0;
+for (int i=0; i < initArray.Length; i++)
+{
+    if (initArray[i].Length <=3) i2++;
+}
+string [] resultArray = new string [i2];
+
+i2 = 0;
+for (int i=0; i < initArray.Length; i++)
+{
+    if (initArray[i].Length <=3) 
+    {
+        resultArray[i2] = initArray[i];
+        i2++;
+    }
+}
+Console.Write("Результирующий массив, содержащий элементы до 3х символов - [ ");
+for (i2 = 0; i2 < resultArray.Length; i2++)
+{
+    Console.Write($"'{resultArray[i2]}' ");
+}
+Console.Write("]");
